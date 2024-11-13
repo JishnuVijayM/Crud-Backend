febd62422461e4d4a3bf75f1f1f535fb5b9c4d63
JishnuVijayM <jishnupmd2002@gmail.com>
2024-11-13 14:30:25 +0530

    Adding cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const crudRouter = require('./router/crudRouter')
 const { connectDb } = require('./utilis/db')
 require('dotenv').config()
 
 const app = express()
 
+app.use(cors()) // Enable CORS
 app.use(express.json())
 
 //Router
@@ -14,7 +16,6 @@ const PORT = process.env.PORT || 3003
 
 connectDb().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server listen on Port: ${PORT}`);
+        console.log(`Server listening on Port: ${PORT}`);
     })
 })
-
